9c8cd78c048f31d335025986948301a228643df3
SamihaHussein2 <74453015+SamihaHussein2@users.noreply.github.com>
2020-12-30 22:42:46 +0200

    Add files via upload

---
+++ ProjectOS22.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectos2;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ *
+ * @author samiha hussein and ragaa moustafa
+ */
+public class ProjectOS22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        input g=new input();
+    }
+    
+}
